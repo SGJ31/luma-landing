487994c590e768ec8b72315ffaebe17bc0fc1f17
Santiago Guzman Jaimes <santi03312005@gmail.com>
2024-07-26 09:56:26 -0500

    The login page with its respective navigation bar is implemented

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,19 @@
+//import { NavLink } from "react-router-dom"
 import Navbar from "../../Components/Navbar"
+import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/solid"
 
 function SignIn() {
     return (
       <div>
         <Navbar/>
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="rounded-lg bg-white/10 w-80 h-80 border border-white/20">
+          <div className="felx flex-col">                   
+            <ArrowLeftEndOnRectangleIcon className="w-6 h-6 text-slate-300"/>
+            <h1 className="text-lg font-medium text-white/90">Welcome to Luma</h1>
+          </div>
+          </div>
+        </div>
       </div>
     )
   }
