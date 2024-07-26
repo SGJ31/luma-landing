8e2e1229a782cd1330f69a12936c15d3e3784e50
Santiago Guzman Jaimes <santi03312005@gmail.com>
2024-07-26 09:30:15 -0500

    time and time zone added to navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,29 @@
 import { NavLink } from "react-router-dom"
+import React, { useState, useEffect } from "react";
 import { SparklesIcon, ArrowUpRightIcon } from "@heroicons/react/24/solid" 
 
 const Navbar = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentTime(new Date());
+        }, 1000);
+    
+        return () => clearInterval(intervalId);
+      }, []);
+
+      const formatTime = (date) => {
+        const hours = date.getHours();
+        const amPm = hours < 12 ? "AM" : "PM";
+        const formattedHours = hours % 12 || 12;
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        return `${formattedHours}:${minutes} ${amPm}`;
+      };
+    
+      const formatTimeZone = () => {
+        return "GMT-5"; 
+      };
 
     return (
         <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
@@ -15,12 +37,14 @@ const Navbar = () => {
                 </li>
             </ul>
             <ul className='flex items-center gap-3'>
+                <li className="text-white/60 text-xs font-semibold">
+                {formatTime(currentTime)} {formatTimeZone()}
+                </li>
                 <li  className='font-semibold text-white/60'>
                     <div className="flex items-center">
                         Explore Events
                         <ArrowUpRightIcon className="w-3 h-3 ml-2"/>  
-                    </div>
-                    
+                    </div>                   
                 </li>
                 <li className='bg-zinc-700 font-bold text-white/60 py-1 px-3 rounded-full'>
                     <NavLink  
